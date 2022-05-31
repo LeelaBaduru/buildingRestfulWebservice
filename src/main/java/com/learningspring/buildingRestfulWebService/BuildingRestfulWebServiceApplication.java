dd7eef65b270be520b1ca06beb5a03fadcc9f1c8
Leela Priyanka Baduru <lbaduru@evoila.de>
2022-05-31 13:09:55 +0200

    Built RESTful WebService which returns Greeting object directly to HTTP response as JSON

---
+++ src/main/java/com/learningspring/buildingRestfulWebService/BuildingRestfulWebServiceApplication.java
@@ -0,0 +1,14 @@
+package com.learningspring.buildingRestfulWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingRestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BuildingRestfulWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningspring/buildingRestfulWebService/BuildingRestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningspring.buildingRestfulWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingRestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
